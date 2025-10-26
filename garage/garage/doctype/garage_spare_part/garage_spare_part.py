050104026df88c19369112f153df3cfdbb4d68d5
AbisenaP <91299253+Abisena@users.noreply.github.com>
2025-10-26 23:06:24 +0700

    feat: surface low stock alerts on spare part dashboard

---
+++ garage/garage/doctype/garage_spare_part/garage_spare_part.py
@@ -0,0 +1,8 @@
+"""DocType controller for Garage Spare Part."""
+
+from frappe.model.document import Document
+
+
+class GarageSparePart(Document):
+    """Basic controller for the Garage Spare Part master."""
+    pass
