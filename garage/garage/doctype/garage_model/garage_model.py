3462bb3e9bd49479d17bd87fab64c22c7eb5caac
AbisenaP <91299253+Abisena@users.noreply.github.com>
2025-12-14 09:01:26 +0700

    Add brand and model doctypes with filtered model links

---
+++ garage/garage/doctype/garage_model/garage_model.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Garage and contributors
+# For license information, please see license.txt
+
+from frappe.model.document import Document
+
+
+class GarageModel(Document):
+    pass
