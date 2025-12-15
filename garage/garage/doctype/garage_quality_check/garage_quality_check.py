0a90cbb9692e81cb51324fac558e7f12f6a866e7
AbisenaP <91299253+Abisena@users.noreply.github.com>
2025-12-15 21:39:58 +0700

    Add Garage Quality Check doctype

---
+++ garage/garage/doctype/garage_quality_check/garage_quality_check.py
@@ -0,0 +1,9 @@
+"""DocType for capturing structured quality control inspections."""
+
+from frappe.model.document import Document
+
+
+class GarageQualityCheck(Document):
+    """Document model for QC inspections with predefined checklist fields."""
+
+    pass
