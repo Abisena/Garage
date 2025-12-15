1cfe0accacba638004c3d636e8e66f31935da4c9
AbisenaP <91299253+Abisena@users.noreply.github.com>
2025-12-15 23:16:26 +0700

    feat: add vehicle handover doctype

---
+++ garage/garage/doctype/vehicle_handover/vehicle_handover.py
@@ -0,0 +1,9 @@
+"""Vehicle Handover DocType for capturing SIKK/permit details."""
+
+from frappe.model.document import Document
+
+
+class VehicleHandover(Document):
+    """Stores vehicle permit data coming from the web app or manual entry."""
+
+    pass
